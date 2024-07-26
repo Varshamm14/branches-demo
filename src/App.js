4d7668780a34a00a7895b195784bdfcf19910935
Varshamm14 <varshamm14@gmail.com>
2024-07-26 16:00:56 +0530

    dev-2 branch :added placement file

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import Attendance from "./Attendance";
 import Classes from "./Classes";
+import Placements from "./Placements";
 
 
 function App() {
   return (
     <>
-    <h1>KodNest Application</h1>
+    <h1>KodNest App</h1>
     <Classes />
     <Attendance />
+    <Placements />
     </>
   );
 }
