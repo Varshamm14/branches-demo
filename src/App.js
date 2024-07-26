84e89cefe8fd626a1cc0462b11009ede21497822
Varshamm14 <varshamm14@gmail.com>
2024-07-26 15:33:40 +0530

    added files

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
+import Attendance from "./Attendance";
+import Classes from "./Classes";
 
 
 function App() {
   return (
     <>
-    <h1>Hello...!</h1>
+    <h1>KodNest Application</h1>
+    <Classes />
+    <Attendance />
     </>
   );
 }
